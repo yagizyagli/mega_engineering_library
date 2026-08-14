2dced6ac0444b5b0fe3d1ee697b74a6f6caa7ad7
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-15 01:06:53 +0300

    Create __init__.py

---
+++ englib/__init__.py
@@ -0,0 +1,10 @@
+"""
+Mega Engineering Library (englib)
+The ultimate, high-performance open-source computing library for 20+ engineering disciplines.
+
+Licensed under the Apache License, Version 2.0.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Yağız Yağlı"
+__license__ = "Apache-2.0"
