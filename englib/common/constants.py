1e278f66d455507ffc965d2851b06e97d1af4c68
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-15 02:25:46 +0300

    Create constants.py

---
+++ englib/common/constants.py
@@ -0,0 +1,16 @@
+"""
+Mega Engineering Library - Universal Common Constants
+All data values are bound strictly to international standard SI units.
+"""
+
+import math
+
+# --- PHYSICAL AND CHEMICAL CONSTANTS ---
+GRAVITY: float = 9.80665                  # Standard acceleration due to gravity (m/s²)
+UNIVERSAL_GAS_CONSTANT: float = 8.314462618  # Ideal gas constant (J/(mol·K))
+AVOGADRO: float = 6.02214076e23           # Avogadro constant (1/mol)
+SPEED_OF_LIGHT: float = 299792458         # Speed of light in vacuum (m/s)
+
+# --- MATHEMATICAL ALIASES ---
+PI: float = math.pi
+E: float = math.e
