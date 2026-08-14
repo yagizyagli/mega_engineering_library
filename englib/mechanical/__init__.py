e04063f3ae2b23deb975aab3eef2110363bc8c72
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-15 01:15:36 +0300

    Update __init__.py

--- englib/mechanical/__init__.py
+++ englib/mechanical/__init__.py
@@ -2,5 +2,6 @@
 Mechanical Engineering core modules including fluid mechanics, thermodynamics, and solid mechanics.
 """
 
-from englib.mechanical.fluid_mechanics import FluidMechanics
 from englib.mechanical.thermodynamics import MechanicalThermodynamics
+from englib.mechanical.solid_mechanics import SolidMechanics
+from englib.mechanical.fluid_mechanics import FluidMechanics
